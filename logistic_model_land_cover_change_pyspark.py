1ceca74f7b60ffe87edd3564631bbec83f02846e
bparment1 <benoit.parmentier@gmail.com>
2019-04-24 14:30:00 -0400

    preparing data for model using vectorassembler

--- logistic_model_land_cover_change_pyspark.py
+++ logistic_model_land_cover_change_pyspark.py
@@ -101,6 +101,7 @@ random_seed = 100 #random seed for reproducibility
 
 ## seting up SPARK
 sc= SparkContext()
+sc = SparkContext.getOrCreate()
 sqlContext = SQLContext(sc)
 
 ######### PART 0: Set up the output dir ################
@@ -139,25 +140,46 @@ freq_val_df = data_df[selected_categorical_var_names].apply(pd.value_counts)
 print(freq_val_df.head())
 values_cat = array(data_df[selected_categorical_var_names].values) #note this is assuming only one cat val here
 
-### Let's read in the information that contains variables
-data_df = pd.read_csv(os.path.join(in_dir,data_fname))
-data_df.columns
+label_encoder = LabelEncoder()  # labeling categories
+one_hot_encoder = OneHotEncoder(sparse=False) #generate dummy variables
+### First integer encode:
+integer_encoded = label_encoder.fit_transform(values_cat)
+
+#onehot_encoded[0:5,]
+print(integer_encoded)
+# Binary encode:
+integer_encoded = integer_encoded.reshape(len(integer_encoded),1)
+print(integer_encoded)
+
+#33 generate dummy variables
+onehot_encoded = one_hot_encoder.fit_transform(integer_encoded)
+print(onehot_encoded)
+onehot_encoded.shape
+type(onehot_encoded)
+
+#Check values generated: invert to check value?
+values_cat[0:5,]
+inverted = label_encoder.inverse_transform([np.argmax(onehot_encoded[1,:])])
+print(inverted)
+
+#assign back to the data.frame
+unique_val = np.array(freq_val_df.index)
+unique_val = np.sort(unique_val)
+print(unique_val)
+names_cat = ['lc_' + str(i) for i in unique_val]
+print(names_cat)
+onehot_encoded_df = pd.DataFrame(onehot_encoded,columns=names_cat)
+onehot_encoded_df.columns
+onehot_encoded_df.head()
+onehot_encoded_df.shape
+data_df.shape
+
+## Add the new encoded variables to the data frame
+data_df= pd.concat([data_df,onehot_encoded_df],sort=False,axis=1)
+data_df.shape
 data_df.head()
 
-################
-##### Step 1: Prepare categorical features/covariates by rescaling values
-
-## Relevant variables used:
-selected_covariates_names = ['land_cover', 'slope', 'roads_dist', 'developped_dist']
-selected_target_names = ['change'] #also called dependent variable
-
-## We need to account for categorical versus continuous variables
-selected_categorical_var_names=['land_cover']
-selected_continuous_var_names=list(set(selected_covariates_names) - set(selected_categorical_var_names))
-##Find frequency of unique values:
-freq_val_df = data_df[selected_categorical_var_names].apply(pd.value_counts)
-print(freq_val_df.head())
-values_cat = array(data_df[selected_categorical_var_names].values) #note this is assuming only one cat val here
+selected_covariates_names_updated = selected_continuous_var_names + names_cat 
 
 ##############
 ## Step 2: Split training and testing and rescaling for continuous variables
@@ -202,15 +224,15 @@ training_spark_df = sqlContext.createDataFrame(X_y_training_df)
 #https://www.guru99.com/pyspark-tutorial.html
 #https://towardsdatascience.com/building-a-linear-regression-with-pyspark-and-mllib-d065c3ba246a
 
-vectorAssembler = VectorAssembler(inputCols = selected_covariates_names, 
+vectorAssembler = VectorAssembler(inputCols = selected_covariates_names_updated, 
                                   outputCol = 'features')
 vtraining_df = vectorAssembler.transform(training_spark_df)
 
-vectorAssembler = VectorAssembler(inputCols = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PT', 'B', 'LSTAT'],
-                                  outputCol = 'features')
-vhouse_df = vectorAssembler.transform(house_df)
-vhouse_df = vhouse_df.select(['features', 'MV'])
-vhouse_df.show(3)
+#vectorAssembler = VectorAssembler(inputCols = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PT', 'B', 'LSTAT'],
+#                                  outputCol = 'features')
+#vhouse_df = vectorAssembler.transform(house_df)
+#vhouse_df = vhouse_df.select(['features', 'MV'])
+#vhouse_df.show(3)
 
 
 from pyspark.ml.regression import LinearRegression
@@ -223,19 +245,22 @@ print("Intercept: " + str(lr_model.intercept))
 #https://www.guru99.com/pyspark-tutorial.html
 #dataset = pd.read_csv("data/AS/test_v2.csv")
 #sc = SparkContext(conf=conf)
-sqlCtx = SQLContext(sc)
-sdf = sqlCtx.createDataFrame(dataset)
+#sqlCtx = SQLContext(sc)
+#sdf = sqlCtx.createDataFrame(dataset)
 
 # Load training data
-training = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
+#training = spark.read.format("libsvm").load("data/mllib/sample_libsvm_data.txt")
 
 lr = LogisticRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
 
-featuresCol = 'features', labelCol='MV'
+#featuresCol = 'features', labelCol='MV'
 
 # Fit the model
 lrModel = lr.fit(training)
-lfModel = lr.fit(X_train.values,y_train.values.ravel())
+lrModel = lr.fit(vtraining_df)
+#
+#
+#lfModel = lr.fit(X_train.values,y_train.values.ravel())
 
 model_logistic = LogisticRegression() #instantiate model object
 model_logistic = model_logistic.fit(X_train.values,y_train.values.ravel())
